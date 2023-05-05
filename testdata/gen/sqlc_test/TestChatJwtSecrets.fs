8790af9fc3fe3ea212a07c4543d3decb2545090a
Hao Wu <wuhaoecho@gmail.com>
2023-05-05 23:36:57 +0800

    Add expecto test (#11)
    * update

---
+++ testdata/gen/sqlc_test/TestChatJwtSecrets.fs
@@ -0,0 +1,31 @@
+module TestsSecrets
+
+open Expecto
+open Npgsql
+open System
+
+[<Tests>]
+let tests =
+
+    let DSN = Environment.GetEnvironmentVariable("DATABASE_URL")
+
+    testList
+        "jwt secrets"
+        [ testCase "can ref code in parent"
+          <| fun _ ->
+              let subject = ChatJwtSecrets.getJwtSecret
+
+              let expected =
+                  """-- name: GetJwtSecret :one
+SELECT id, name, secret, audience FROM jwt_secrets WHERE name = @name
+"""
+
+              Expect.equal subject expected "I compute, therefore I am."
+
+
+          testCase "get secrets"
+          <| fun _ ->
+              use conn = new NpgsqlConnection(DSN)
+              let subject = ChatJwtSecrets.GetJwtSecret conn "my-jwt-secret"
+
+              Expect.equal subject.Head.Audience "my-app" "app name ok" ]
